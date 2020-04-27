8fbd4b80531bfe5ebe28df22bbb9687795a78776
aitorllj93 <aitorllj93@gmail.com>
2020-04-27 13:58:32 +0200

    initial commit

---
+++ projects/ngx-sounds/src/lib/ngx-sounds.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { NgxSoundsComponent } from './ngx-sounds.component';
+
+
+
+@NgModule({
+  declarations: [NgxSoundsComponent],
+  imports: [
+  ],
+  exports: [NgxSoundsComponent]
+})
+export class NgxSoundsModule { }
---
+++ projects/ngx-sounds/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-sounds
+ */
+
+export * from './lib/ngx-sounds.service';
+export * from './lib/ngx-sounds.module';

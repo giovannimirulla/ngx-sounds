787eefb0e4ebf094a1c2a67b347a2e759b51c035
Giovanni Mirulla <mirullagiovanni@gmail.com>
2022-07-20 14:18:58 +0200

    Update ngx-sounds.service.d.ts

--- lib/ngx-sounds.service.d.ts
+++ lib/ngx-sounds.service.d.ts
@@ -1,10 +1,11 @@
 import * as ɵngcc0 from '@angular/core';
+import { Howl } from 'howler';
 export declare class SoundsService {
     howler: HowlerGlobal;
     set volume(value: number);
     get volume(): number;
     mute(value?: boolean): void;
-    howl(options: IHowlProperties): Howl;
+    howl(options: any): Howl;
     play(src: string): import("rxjs").Observable<unknown>;
     static ɵfac: ɵngcc0.ɵɵFactoryDeclaration<SoundsService, never>;
     static ɵprov: ɵngcc0.ɵɵInjectableDeclaration<SoundsService>;
